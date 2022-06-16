8e94c8cb38041eb3163b0761758a6ea3a4b7030c
Noxty <eleazaargarcia@outlook.com>
2022-06-15 23:39:52 -0500

    Tarea Modulo 12
    Se realizaron todos los puntos de la tarea 12

---
+++ Proyecto Breakout/Assets/Scenes/_Scripts/Jugador.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+    [SerializeField] public int limiteX = 23;
+    [SerializeField] public float velocidadPaddle = 30f;
+
+    Vector3 mousePos2D;
+    Vector3 mousePos3D;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //mousePos2D = Input.mousePosition;
+        //mousePos3D.z = -Camera.main.transform.position.z;
+        //mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    //mueve el objeto a la derecha
+        //    this.transform.Translate(Vector3.down * velocidadPaddle * Time.deltaTime);
+        //}
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    //mueve el obejto a la Izquierda
+        //    this.transform.Translate(Vector3.up * velocidadPaddle * Time.deltaTime);
+
+        //}
+        this.transform.Translate(Input.GetAxis("Horizontal") * Vector3.down * velocidadPaddle * Time.deltaTime);
+
+        Vector3 pos = this.transform.position;
+        //pos.x = mousePos3D.x;
+        if(pos.x < -limiteX)
+        {
+            pos.x = -limiteX;
+
+        }else if(pos.x > limiteX)
+        {
+            pos.x = limiteX;
+
+        }
+        transform.position = pos;
+
+    }
+}
