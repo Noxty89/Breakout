bbda9c9ee2d13f27255825b408716bf77dc4e914
Noxty <eleazaargarcia@outlook.com>
2022-07-10 21:56:13 -0500

    Avance Modulo 14 2.0
    Se agregaron los scripts a los botones de menú inicial y menú opciones.

---
+++ Proyecto Breakout/Assets/_Scripts/Bloque.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Bloque : MonoBehaviour
+{
+    public int Resistencia = 1;
+    public UnityEvent AumentarPuntaje;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Bola")
+        {
+            RebotarBola(collision);
+        }
+    }
+
+    public virtual void RebotarBola(Collision collision)
+    {
+        Vector3 direccion = collision.contacts[0].point - transform.position;
+        direccion = direccion.normalized;
+        collision.rigidbody.velocity = collision.gameObject.GetComponent<Bola>().velocidadBola * direccion;
+        Resistencia--;
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Resistencia <= 0)
+        {
+            AumentarPuntaje.Invoke();
+            Destroy(this.gameObject);
+        }
+    }
+    public virtual void RebotarBola()
+    {
+
+    }
+}
---
+++ Proyecto Breakout/Assets/_Scripts/Bloque_Agua.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloque_Agua : Bloque
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Resistencia = 1;
+        //pero al destruitr el bloque se realentiza la bola y cambia su direccion aleatoriamente
+    }
+
+  }
